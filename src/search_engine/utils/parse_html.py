01662b57fb45e1b4b6c248e70c9656f04254fe87
Priyanshu <priyanshu@Kali.Priyanshu>
2026-01-13 20:11:06 +0530

    Util file to fetch content from html added

---
+++ src/search_engine/utils/parse_html.py
@@ -0,0 +1,22 @@
+"""
+This file will contain the code to extaract useful information from the crawled web pages
+"""
+
+from typing import List
+from bs4 import BeautifulSoup
+
+
+def extract_outgoing_links(soup: BeautifulSoup) -> List[str]:
+    """
+    This method will extract all the outgoing links from the given BeautifulSoup object.
+
+    Args:
+        soup (BeautifulSoup): The BeautifulSoup object representing the HTML content.
+
+    Returns:
+        List[str]: A list of outgoing links found in the HTML content.
+    """
+
+    links = soup.find_all('a', href=True)
+    logger.debug(f"Found {len(links)} links in the HTML content: {links}")
+    return links
