65df490805418f941583df87e5551f93fde59831
priyanshu <mehtapriyanshu408@gmail.com>
2026-01-13 22:36:22 +0530

    variables updated

--- src/search_engine/utils/variables.py
+++ src/search_engine/utils/variables.py
@@ -2,6 +2,8 @@
 This class will contain some common gobal variables used across the search engine project.
 """
 
+from pathlib import Path
+
 class CommonVariables:
     """
     Initialize the variables module.
@@ -10,5 +12,7 @@ class CommonVariables:
     SEED_URLS = [
         "https://en.wikipedia.org/wiki/Main_Page",
     ]
-    MAX_LIMIT = 100
-    MAX_DEPTH = 5
+    MAX_LIMIT = 5000
+
+    ROOT_DIR = Path(__file__).parent.parent.parent
+    JSONL_FILE_PATH = ROOT_DIR / "src" / "search_engine" / "models" / "PageModel.jsonl"
